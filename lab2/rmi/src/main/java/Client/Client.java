fb9bc403a48ad03f64a3f84a7bdbb3fb3bd5acec
Diego Cárdenas <102341495+diegomated1@users.noreply.github.com>
2024-02-06 07:14:32 -0500

    feat: rmi server and client

---
+++ lab2/rmi/src/main/java/Client/Client.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Client;
+
+import Interfaces.ISongProvider;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author 000430063
+ */
+public class Client extends Thread {
+    
+    private final String host;
+    private final int port;
+    private ISongProvider songProvider;
+    public Runnable OnStart;
+    
+    public Client(){
+        this.host = "localhost";
+        this.port = 3000;
+    }
+    public Client(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            Registry registry = LocateRegistry.getRegistry(this.host, this.port);
+            this.songProvider = (ISongProvider) registry.lookup("SongProvider");
+            if(this.OnStart != null){
+                this.OnStart.run();
+            }
+            registry.unbind(host);
+        } catch (NotBoundException | RemoteException e) {
+        }
+    }
+    
+    public ISongProvider getSongProvider() {
+        return this.songProvider;
+    }
+    
+    public void close() {
+        this.interrupt();
+    }
+
+}
---
+++ lab2/rmi/src/main/java/Interfaces/ISong.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaces;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 000430063
+ */
+public interface ISong extends Serializable {
+    String getTitle();
+    String getGenre();
+    String getAuthor();
+    String getLanguage();
+    String getLaunchYear();
+    @Override
+    String toString();
+}
